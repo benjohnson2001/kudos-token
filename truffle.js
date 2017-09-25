81603931af26b6863385d091b989ef28228941b3
Ben <ben@letskedaddle.com>
2017-09-25 11:47:03 -0400

    added code coverage functionality

--- truffle.js
+++ truffle.js
@@ -8,11 +8,19 @@ module.exports = {
          port: 8545,
          network_id: "*" // Match any network id
       },
+      coverage: {
+         host: "localhost",
+         network_id: "*",
+         port: 8555,
+         gas: 0xfffffffffff,
+         gasPrice: 0x01
+      },
       testnet: {
          host: "localhost",
          port: 8545,
          network_id: 4,
-         from: "0x8550b1cc5914909d75c12708b92049ab78b52ffe",
+         from: "0x4F3Da41D2adb81e8e3A808E865b55c68f163b81A",
+         // from: "0x8550b1cc5914909d75c12708b92049ab78b52ffe",
          gas: 4612388
       },
       production: {
